bda5c2b347f6b68d8a67d0eca72d7d19cee59cf6
destnayu <116272811+destnayu@users.noreply.github.com>
2022-10-25 22:11:34 +0700

    Update Problem6DrawXYZ.java
    Revisi pada tanggal 25/10/2022 22:11 WIB

--- basic-programming/Problem6DrawXYZ.java
+++ basic-programming/Problem6DrawXYZ.java
@@ -3,33 +3,25 @@ import java.util.Scanner;
 public class Problem6DrawXYZ {
     private static void DrawXYZ(int n){
         Scanner input = new Scanner(System.in);
-
+        int angka=0;
         System.out.print("Input: ");
         n = input.nextInt();
 
-        tampil(n);
-    }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                angka++;
 
-    public static void  tampil(int n) {
-        for (int i = 0; i <= n; i++) {
-            System.out.print("Y");
-            for (int j = i; j < n; j++) {
-                System.out.print("Y");
-            }
-
-            for (int j = i; j > 0; j--) {
-                if (j % 2 == 0) {
+                if (angka % 3 == 0) {
+                    System.out.print("X");
+                } else if (angka % 2 == 1) {
+                    System.out.print("Y");
+                } else if (angka % 2 == 0) {
                     System.out.print("Z");
                 }
             }
-            for (int x = 3; x < n; x+=3) {
-                if (x % 5 != 0) {
-                    System.out.print("X");
-                }
-            }
-
             System.out.println();
         }
+        System.out.println();
     }
 
     public static void main(String[] args) {
